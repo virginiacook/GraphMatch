91b48f352b0e98f5771bebbf9138501c13a1d516
virginiacook <virg.cook@earthlink.net>
2013-12-19 00:33:22 -0500

    added graphy file

--- graphy.cpp
+++ graphy.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Please enter your difficuly level" << endl;
+	cout << "Please enter your difficuly level (1-10)" << endl;
 	int difficulty;
 	cin >> difficulty;
 	cout << endl;
@@ -13,6 +13,7 @@ int main()
 	int numSlopes = 10;
 	double slopes [10] = {2.3,43,4.2,7.4,23,45,65.3,95,23,1};
 	for(int n=0;n<10;n++){
+		slopes[n] = rand()%(difficulty*2) - difficulty;
 		cout << slopes[n] << endl;
 	}
 	return 0;
